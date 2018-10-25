1c1c451d65c81703e107826cf6194d0d4c46b200
Cira Devlin <cira.devlin@leveluprpo.com>
2018-10-25 20:17:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ function updateDriverWithKeyAndValue(driver, key, value) {
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
 driver.name = 'Sam'
 driver.address = '12 Broadway'
+return driver 
 
 }
 
