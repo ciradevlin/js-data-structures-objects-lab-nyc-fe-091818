55fbf02df339b610057c2dc9011e6564a15f25e1
Cira Devlin <cira.devlin@leveluprpo.com>
2018-10-25 20:06:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your solution in this file!
+const driver = {
+}
+
+function updateDriverWithKeyAndValue(driver, key, value) {
+  const newDriver = {...driver}
+  newDriver.address = '11 Broadway'
+  return newDriver
+}
+
+
